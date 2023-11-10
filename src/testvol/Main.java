6234b90fa0159c57b5d590fb13cb5dd1dd6a1a78
asmaachouai <67027733+asmaachouai@users.noreply.github.com>
2023-11-10 13:32:04 +0100

    Premier commit

---
+++ src/testvol/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testvol;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Passager p =new Passager("g114", "num2674", "num111"); 
+        System.out.println(p);
+    }
+
+}
